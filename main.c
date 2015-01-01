4b7ccfe2077859bea70e4e1c0e34c7e694a9d9af
sashashan <sashashan@users.noreply.github.com>
2015-01-01 16:14:25 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "reader.h"
+#include "scanner.h"
+#include "parser.h"
+
+int main (int argc, char *argv[]) {
+    FILE *fp;
+    
+    if (argc < 2) {
+        printf("Usage: ./format infile\n");
+        exit(1);
+    }
+    
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        printf("Error: infile.txt does not exist.\n");
+        exit(1);
+    }
+  
+    initialize_reader(fp);
+    
+    // SASHA's error tasting
+    
+    // Do scan, parse, evaluate here 
+    parse();
+    
+    finalize_reader();
+    
+    exit(0);
+}
